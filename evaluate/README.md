8e72e3371228870220a8eedc2f6254b2d5def1be
gmftbyGMFTBY <18811371908@163.com>
2022-04-09 17:15:55 +0800

    Sat Apr  9 17:15:55 CST 2022

---
+++ evaluate/README.md
@@ -0,0 +1,17 @@
+## Obtain the Evaluation Results
+
+### 1. get the stanford models
+
+```bash
+./get_stanford_models.sh
+```
+
+### 2. change the inference results
+
+change the path of the inference results file in the cocoeval.py
+
+### 3. evaluate
+
+```bash
+python cocoeval.py
+```
--- evaluate/cocoeval.py
+++ evaluate/cocoeval.py
@@ -4,9 +4,8 @@ import json
 from json import encoder
 encoder.FLOAT_REPR = lambda o: format(o, '.3f')
 
+# read the results file
 cocoEval = COCOEvalCap('../inference_result/magic_small_model_k_10_alpha_0.6_beta_2.0.json')
-# cocoEval.params['image_id'] = cocoRes.getImgIds()
 cocoEval.evaluate()
-# print output evaluation scores
 for metric, score in cocoEval.eval.items():
     print ('%s: %.3f'%(metric, score))
