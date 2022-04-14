aaf08405629fc1143b9571c5f0cbc63623eaf3f3
Yixuan Su <syxistheone@gmail.com>
2022-04-14 21:46:59 +0100

    Delete source_flickr_target_mscoco_clip_retrieval.sh

--- image_captioning/clip/source_flickr_target_mscoco_clip_retrieval.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python clipretrieval.py\
-    --clip_name openai/clip-vit-base-patch32\
-    --test_image_prefix_path ../data/mscoco/test_images/\
-    --test_path ../data/mscoco/mscoco_test.json\
-    --index_matrix_path ./flickr30k_index/index_matrix.txt\
-    --mapping_dict_path ./flickr30k_index/text_mapping.json\
-    --save_path_prefix ../inference_result/flickr30k_model_to_mscoco/\
-    --save_name source_flickr30k_target_mscoco_clip_retrieval.json
