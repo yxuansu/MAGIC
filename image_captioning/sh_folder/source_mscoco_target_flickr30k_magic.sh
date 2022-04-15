174185255a73605bc754bb2df1eaca755ce7a410
Yixuan Su <syxistheone@gmail.com>
2022-04-15 10:21:19 +0100

    Add files via upload

---
+++ image_captioning/sh_folder/source_mscoco_target_flickr30k_magic.sh
@@ -0,0 +1,13 @@
+CUDA_VISIBLE_DEVICES=1 python ../inference_magic.py\
+    --language_model_code_path ../language_model/\
+    --language_model_name cambridgeltl/magic_mscoco\
+    --clip_path ../clip/\
+    --clip_name openai/clip-vit-base-patch32\
+    --test_image_prefix_path ../data/flickr30k/test_images/\
+    --test_path ../data/flickr30k/flickr30k_test.json\
+    --decoding_len 16\
+    --k 45\
+    --alpha 0.1\
+    --beta 2.0\
+    --save_path_prefix ../inference_result/mscoco_model_to_flickr30k/\
+    --save_name magic_result.json
