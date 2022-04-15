400bc3baee1ee7e912ecf53c866fbbd4b2aa6c8a
Yixuan Su <syxistheone@gmail.com>
2022-04-15 10:50:42 +0100

    Add files via upload

---
+++ image_captioning/sh_folder/source_mscoco_target_flickr30k_contrastive_baseline.sh
@@ -0,0 +1,11 @@
+CUDA_VISIBLE_DEVICES=1 python ../inference_baseline.py\
+    --language_model_code_path ../language_model/\
+    --language_model_name cambridgeltl/magic_mscoco\
+    --test_path ../data/flickr30k/flickr30k_test.json\
+    --decoding_method contrastive\
+    --decoding_len 16\
+    --k 45\
+    --alpha 0.1\
+    --save_path_prefix ../inference_result/mscoco_model_to_flickr30k/\
+    --save_name contrastive_result.json
+
