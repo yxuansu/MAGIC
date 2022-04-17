6df23d4ca2867024e3fcd06d391ebc52d18f2c2e
Yixuan Su <syxistheone@gmail.com>
2022-04-17 14:25:41 +0100

    Update mscoco_zerocap.sh

--- image_captioning/zerocap/mscoco_zerocap.sh
+++ image_captioning/zerocap/mscoco_zerocap.sh
@@ -10,5 +10,5 @@ CUDA_VISIBLE_DEVICES=1 python run.py \
     --lm_model cambridgeltl/magic_mscoco \
     --test_image_prefix_path ../data/mscoco/test_images \
     --test_path ../data/mscoco/mscoco_test.json \
-    --save_path_prefix ../inference_result/mscoco/baselines \
+    --save_path_prefix ../inference_result/mscoco/baselines/ \
     --save_name zerocap_result.json
